e253b4f97e4a1475e31e061bdbbb1ee70dfb93a2
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-08 15:40:02 +0300

    edit migrate command

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
-from app import create_app
+from app import create_app,db
 
 from flask_script import Manager, Server 
+from app.models import User
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
 
 manager =  Manager(app)
 manager.add_command('server',Server)
+migrate =  Migrate(app)
+manager.add_command('db',MigrateCommand)
+
 
 @manager.command
 def test():
@@ -15,6 +20,10 @@ def test():
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
 	manager.run()

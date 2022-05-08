31def1d67a70629daac1d9dde6906dd66fccad01
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-08 15:40:50 +0300

    edit database path

--- config.py
+++ config.py
@@ -3,8 +3,20 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lynne:lynne2022@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lynne:lynne2022@localhost/perfectpitches'
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitches'
+    SENDER_EMAIL = 'linetlucy21@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     pass

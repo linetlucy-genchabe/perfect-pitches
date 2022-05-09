77b49e17240f1028d1645ca8b33e3992a34c0825
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-09 17:02:48 +0300

    styling for html pages and add ui image

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 
+
 from . import main
 from .forms import PostForm, CommentForm,UpdateProfile
 from ..models import Post, Comment, User,Upvote,Downvote

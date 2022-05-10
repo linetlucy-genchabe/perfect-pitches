20b752cfbe732f39283330c603020b3f258eba0e
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-10 17:57:17 +0300

    deployment

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://nrwiupmrkgvosu:80444bc323576edb15cf4d514ac117af21c1c5cd1b761800a1df818f7f392c02@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d8ii2ma1pjbpdm'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lynne:lynne2022@localhost/perfectpitches'
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -19,7 +19,7 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://nrwiupmrkgvosu:80444bc323576edb15cf4d514ac117af21c1c5cd1b761800a1df818f7f392c02@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d8ii2ma1pjbpdm'
     pass
 
 
